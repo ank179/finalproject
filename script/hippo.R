4cd3a41a096fafcd170660c2b27886b04420ee8c
Ananya Kapur <ank179@pitt.edu>
2019-11-25 10:36:25 -0500

    loading data

---
+++ script/hippo.R
@@ -0,0 +1,8 @@
+#changing working directory 
+getwd()
+setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/script")
+
+
+#loading data 
+setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/data")
+hipp <- read.csv("hippvolume.csv")
