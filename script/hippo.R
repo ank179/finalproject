0eb1105898ab023a8f5b522aaf19ab0277842a61
Ananya Kapur <ank179@pitt.edu>
2019-11-26 16:00:52 -0500

    ANOVA analysis of data

--- script/hippo.R
+++ script/hippo.R
@@ -6,5 +6,16 @@ setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/script")
 #loading data 
 setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/data")
 hipp <- read.csv("hippvolume.csv")
+
+#summary of hipp data 
 summary(hipp)
 
+#analysis 
+
+setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/results")
+hipp$group <- factor(hipp$group, levels = c("no seizures", "no CFS", "CFS")) 
+hipp
+hippAnova <- lm(hipp$hippoVolumeRatio ~ group, data = hipp)
+anova(hippAnova)
+out <- capture.output(anova(hippAnova))
+cat("hippAnova_results", out, file="hippAnova_results.txt", sep="n", append=TRUE)
