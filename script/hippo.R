eda470c138fe9035d0db21a83007b412b78fe4c0
Ananya Kapur <ank179@pitt.edu>
2019-11-26 16:06:23 -0500

    making the strip chart

--- script/hippo.R
+++ script/hippo.R
@@ -19,3 +19,14 @@ hippAnova <- lm(hipp$hippoVolumeRatio ~ group, data = hipp)
 anova(hippAnova)
 out <- capture.output(anova(hippAnova))
 cat("hippAnova_results", out, file="hippAnova_results.txt", sep="n", append=TRUE)
+
+#making strip charts 
+setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/results")
+pdf("hipp_plot.pdf")
+graph <- stripchart(hipp$hippoVolumeRatio ~ hipp$group, data = hipp, method = "jitter",
+                    vertical = TRUE, ylab = "Hippocampus Volume Ratio (%)", xlab = "Patients", col = c("coral3", "darkslategrey", "darkolivegreen"), cex = 1.5, 
+                    main = "Hippocampus Volume Ratio for Patients", pch=16)
+meanHipVol = tapply(hipp$hippoVolumeRatio, hipp$group, mean)
+segments(x0 = c(1,2,3) - 0.1, y0 = meanHipVol, x1 = c(1,2,3) + 0.1, 
+         y1 = meanHipVol, lwd = 2)
+dev.off()
