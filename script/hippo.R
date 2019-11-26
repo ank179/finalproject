12158a47fb28625d4f6e8c150835dc75b5c665f6
Ananya Kapur <ank179@pitt.edu>
2019-11-26 15:58:13 -0500

    summary of hipp data

--- script/hippo.R
+++ script/hippo.R
@@ -6,3 +6,5 @@ setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/script")
 #loading data 
 setwd("/Users/anyakapur/Desktop/Biostat/Finalproject/data")
 hipp <- read.csv("hippvolume.csv")
+summary(hipp)
+
